d6c5d471a7c035a71a44ecda4cbd2066b2889be7
7A Laurits Schou <laur458b@mkfonline.dk>
2018-01-31 18:25:03 +0100

    denne kode er til robo maze

---
+++ MaZerobot/MaZerobot/MaZerobot.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MaZerobot.ino
+ Created:	1/31/2018 6:23:43 PM
+ Author:	Romvi
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
